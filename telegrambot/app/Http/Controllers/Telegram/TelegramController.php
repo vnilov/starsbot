8062c95cb07e637af64bfa286a6d78a39ab06224
Nilov Vadim <vnilov@ctcmedia.ru>
2016-04-05 13:03:59 +0300

    controller

--- telegrambot/app/Http/Controllers/Telegram/TelegramController.php
+++ telegrambot/app/Http/Controllers/Telegram/TelegramController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers\Telegram;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
 use App\Telegram\TelegramAPI;
 use Log;
 
 class TelegramController extends Controller
 {
-    public function getUpdates()
+    public function getUpdates(Request $request)
     {
-        $input = file_get_contents('php://input');
+        //$input = file_get_contents('php://input');
+        $input = $request->all();
         Log::info($input);
     }
 }
